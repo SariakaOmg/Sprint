eb65173aee95cdbf3dd38a99c3644e51790eed17
SariakaOmg <isaro3465@gmail.com>
2026-07-12 14:53:36 +0300

    Sprint-5

--- testFramework/src/main/java/exemple/ex1.java
+++ testFramework/src/main/java/exemple/ex1.java
@@ -1,20 +1,33 @@
 package exemple;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.print.DocFlavor.STRING;
+
 import mg.itu.Controller;
 import mg.itu.URLMapping;
+import utils.*;
 
 @Controller
 public class ex1 {
-    @URLMapping(value="/sasa1", methode = "GET")
+    @URLMapping(value="/sasa2", methode = "GET")
     public String fonctionExemple(){
         // System.out.println("OK11");
         String a = "ok methode 1 ";
         return a;
     }
 
-    @URLMapping(value="/sasa1", methode = "POST")
-    public String methode2(){
-       // System.out.println("methode2");
-       String g = "ok methode 2 ";
-       return g;
+    @URLMapping(value="/sasa1", methode = "GET")
+    public ModelView methode2(){
+        ArrayList<String> fruitss = new ArrayList<>();
+        fruitss.add("Maka");
+        ArrayList<Object> fruits = new ArrayList<>(fruitss);
+        Map<String, ArrayList<Object>> list = new HashMap<>();
+        list.put("key", fruits);
+        ModelView mv = new ModelView("Fafa",list);
+        
+        return mv;
     }
+
 }
