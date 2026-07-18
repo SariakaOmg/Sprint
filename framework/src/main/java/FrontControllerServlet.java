e0bf709a87b20f8b0560e7c4feea78def474ba0f
SariakaOmg <isaro3465@gmail.com>
2026-07-18 16:23:46 +0300

    ok

--- framework/src/main/java/FrontControllerServlet.java
+++ framework/src/main/java/FrontControllerServlet.java
@@ -24,7 +24,13 @@ public class FrontControllerServlet extends jakarta.servlet.http.HttpServlet {
     private String extensionView;
  
     public void init() throws ServletException {
-        String chemine = "/opt/tomcat/webapps/testFramework/WEB-INF/classes";
+        // Étape A : Récupérer la valeur déclarée dans le web.xml ("/WEB-INF/classes/")
+        String parametreChemin = this.getInitParameter("CheminClasses");
+
+        // Étape B : Convertir en chemin absolu réel sur le disque
+        String chemine = this.getServletContext().getRealPath(parametreChemin);
+        
+        // String chemine = "/opt/tomcat/webapps/testFramework/WEB-INF/classes";
         String packageContr = "";
         if (getInitParameter("PackCon") != null) {
             packageContr = getInitParameter("PackCon");
